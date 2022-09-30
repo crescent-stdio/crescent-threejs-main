b88c6b3e54a97b715bd98f6b50cc623b187b5b4f
Hannah Park <hannah@crescent.dev>
2022-09-30 16:58:56 +0900

    Change main link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cube from './components/Cube';
 function App() {
   return (
     <div className="w-full min-h-full">
-      <a href="https://blog.crescent.dev" target="_blank" rel="noreferrer">
+      <a href="https://www.crescent.studio" target="_blank" rel="noreferrer">
         <h1 className="text-2xl font-bold p-4 text-yellow-300">CRESCENT ☾</h1>
       </a>
       <Cube />
